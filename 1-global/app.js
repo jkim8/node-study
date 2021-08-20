a093b858369a0e64b1edccd5a3c4b2ebd1b76f16
jkim8 <juno0218@naver.com>
2021-08-20 08:47:12 -0400

    module (exports and require)

--- 1-global/app.js
+++ 1-global/app.js
@@ -1,11 +1,3 @@
-const fs = require('fs')
+console.log('loading');
 
-console.log(global); //브라우저에서는 window가 global 객체이지만 노드는 global 이 global 객체이다
-
-global.hello = () => {
-    console.log('hello');
-    gobal.console.log('hello');  // 이 두개는 같은 것이다 
-}
-
-global.hello()
-hello()       // 이 두개는 같은 것이다 
+console.assert(2 === 3, 'same')
