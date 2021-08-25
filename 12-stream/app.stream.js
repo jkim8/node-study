9d3ac0677a500cde73a18fcef329f96560d2fefc
jkim8 <juno0218@naver.com>
2021-08-25 09:13:43 -0400

    stream

---
+++ 12-stream/app.stream.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const data = []
+const readStream = fs.createReadStream('./file.txt', {
+    highWaterMark: 8,
+    // encoding: 'utf-8',
+}).once('data', (chunk)=>{
+    // console.log(chunk);
+    data.push(chunk)
+    console.count('data');
+}).on('end', ()=>{
+    console.log(data.join(''));
+}).on('error', (error) => {
+    console.log(error);
+})
